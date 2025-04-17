0291b2d59de8c045d4486df68b2018714411fef5
priyanka108 <68846250+priyanka108@users.noreply.github.com>
2025-04-16 23:57:10 -0400

    Update utiils.py

--- utiils.py
+++ utiils.py
@@ -1,1 +1,12 @@
+import pandas as pd
+
+def load_data(filepath):
+    try:
+        df = pd.read_csv(filepath)
+        return df
+    except Exception as e:
+        print(f"Error loading {filepath}: {e}")
+        return pd.DataFrame()
+
+
 
